f6cb2daa521ff95c9e63869b7ce1dc8f26240fe0
root <root@ip-10-0-93-91.ec2.internal>
2021-09-29 11:14:19 +0000

    Copy Template Repo

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+function MyApp({ Component, pageProps }) {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: '/graphql',
+  })
+  return (
+    <div className="bg-white text-black font-display flex flex-col items-center">
+      <ApolloProvider client={client}>
+        <div className="py-10 w-full max-w-[90vw] lg:max-w-[75vw] sm:px-10 flex flex-col">
+          <Component {...pageProps} />
+        </div>
+      </ApolloProvider>
+    </div>
+  )
+}
+
+export default MyApp
